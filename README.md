464aafc94fc681a3de3b106d304e8a54de36f0ad
Rafael Alberto <rafael.alberto1703@gmail.com>
2022-02-19 13:44:20 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kafka Studies
+
+The reason of this app is to learn how to work with Kafka. <br/>
---
+++ src/main/java/br/com/kafka/App.java
@@ -0,0 +1,9 @@
+package br.com.kafka;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Kafka!");
+    }
+
+}
